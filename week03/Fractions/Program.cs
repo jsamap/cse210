eadefce1c93894e09b99c0287783475727594a01
jsamap <hsml-@hotmail.com>
2025-09-15 18:41:49 -0600

    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction f1 = new();
+        Fraction f2 = new(5);
+        Fraction f3 = new(3, 4);
+        Fraction f4 = new(1, 3);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
